318bb0d5c67ab567a3d30c1f7f89fe6ab0d7ee07
H M Shariar Khan  Shuvho <shariar.shuvho@FN-LP-224.local>
2024-04-25 14:47:09 +0600

    code refactoring

--- controllers/reporterController.js
+++ controllers/reporterController.js
@@ -67,108 +67,157 @@ exports.getReport = async (req, res) => {
             pwaScore: (sum.pwaScore / data.length).toFixed(2) + '%',
             loadingExperience: {}
         };
+        //     if (key === 'CUMULATIVE_LAYOUT_SHIFT_SCORE') {
+        //         let percentile = sum.loadingExperience[key] / data.length;
+        //         let category = '';
+        //         if (percentile <= 0.1) {
+        //             category = 'Good';
+        //         } else if (percentile > 0.1 && percentile <= 0.25) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + '%',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 0.1), Needs Improvement (0.1 - 0.25), Poor (> 0.25)'
+        //         };
+        //     } else if (key === 'TIME_TO_FIRST_BYTE') {
+        //         let percentile = sum.loadingExperience[key] / data.length;
+        //         let category = '';
+        //         if (percentile <= 0.8) {
+        //             category = 'Good';
+        //         } else if (percentile > 0.8 && percentile <= 1.8) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + 's',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 0.8 s), Needs Improvement (0.8 - 1.8 s), Poor (> 1.8 s)'
+        //         };
+        //     } else if (key === 'FIRST_CONTENTFUL_PAINT') {
+        //         let percentile =
+        //             sum.loadingExperience[key] / data.length / 1000;
+        //         let category = '';
+        //         if (percentile <= 0.8) {
+        //             category = 'Good';
+        //         } else if (percentile > 0.8 && percentile <= 1.8) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + 's',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 1.8 s), Needs Improvement (1.8 - 3.0 s), Poor (> 3.0 s)'
+        //         };
+        //     } else if (key === 'FIRST_INPUT_DELAY') {
+        //         let percentile = sum.loadingExperience[key] / data.length;
+        //         let category = '';
+        //         if (percentile <= 100) {
+        //             category = 'Good';
+        //         } else if (percentile > 100 && percentile <= 300) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + 'ms',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 100 ms), Needs Improvement (100 - 300 ms), Poor (> 300 ms)'
+        //         };
+        //     } else if (key === 'INTERACTION_TO_NEXT_PAINT') {
+        //         let percentile = sum.loadingExperience[key] / data.length;
+        //         let category = '';
+        //         if (percentile <= 100) {
+        //             category = 'Good';
+        //         } else if (percentile > 100 && percentile <= 300) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + 'ms',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 100 ms), Needs Improvement (100 - 300 ms), Poor (> 300 ms)'
+        //         };
+        //     } else if (key === 'LARGEST_CONTENTFUL_PAINT') {
+        //         let percentile = sum.loadingExperience[key] / data.length;
+        //         let category = '';
+        //         if (percentile <= 2.5) {
+        //             category = 'Good';
+        //         } else if (percentile > 2.5 && percentile <= 4.0) {
+        //             category = 'Needs Improvement';
+        //         } else {
+        //             category = 'Poor';
+        //         }
+        //         average.loadingExperience[key] = {
+        //             percentile: percentile.toFixed(2) + 's',
+        //             category: category,
+        //             explanation:
+        //                 'Good (≤ 2.5 s), Needs Improvement (2.5 - 4.0 s), Poor (> 4.0 s)'
+        //         };
+        //     }
+        // });
+
+        const metrics = {
+            CUMULATIVE_LAYOUT_SHIFT_SCORE: {
+                good: 0.1,
+                needsImprovement: 0.25,
+                unit: '%'
+            },
+            TIME_TO_FIRST_BYTE: { good: 0.8, needsImprovement: 1.8, unit: 's' },
+            FIRST_CONTENTFUL_PAINT: {
+                good: 0.8,
+                needsImprovement: 1.8,
+                unit: 's',
+                scale: 1000
+            },
+            FIRST_INPUT_DELAY: { good: 100, needsImprovement: 300, unit: 'ms' },
+            INTERACTION_TO_NEXT_PAINT: {
+                good: 100,
+                needsImprovement: 300,
+                unit: 'ms'
+            },
+            LARGEST_CONTENTFUL_PAINT: {
+                good: 2.5,
+                needsImprovement: 4.0,
+                unit: 's'
+            }
+        };
+
+        const getCategory = (percentile, { good, needsImprovement }) => {
+            if (percentile <= good) {
+                return 'Good';
+            } else if (percentile > good && percentile <= needsImprovement) {
+                return 'Needs Improvement';
+            } else {
+                return 'Poor';
+            }
+        };
 
         Object.keys(sum.loadingExperience).forEach((key) => {
-            if (key === 'CUMULATIVE_LAYOUT_SHIFT_SCORE') {
+            const metric = metrics[key];
+            if (metric) {
                 let percentile = sum.loadingExperience[key] / data.length;
-                let category = '';
-                if (percentile <= 0.1) {
-                    category = 'Good';
-                } else if (percentile > 0.1 && percentile <= 0.25) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
+                if (metric.scale) {
+                    percentile /= metric.scale;
                 }
+                const category = getCategory(percentile, metric);
                 average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + '%',
+                    percentile: percentile.toFixed(2) + metric.unit,
                     category: category,
-                    explanation:
-                        'Good (≤ 0.1), Needs Improvement (0.1 - 0.25), Poor (> 0.25)'
-                };
-            } else if (key === 'TIME_TO_FIRST_BYTE') {
-                let percentile = sum.loadingExperience[key] / data.length;
-                let category = '';
-                if (percentile <= 0.8) {
-                    category = 'Good';
-                } else if (percentile > 0.8 && percentile <= 1.8) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
-                }
-                average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + 's',
-                    category: category,
-                    explanation:
-                        'Good (≤ 0.8 s), Needs Improvement (0.8 - 1.8 s), Poor (> 1.8 s)'
-                };
-            } else if (key === 'FIRST_CONTENTFUL_PAINT') {
-                let percentile =
-                    sum.loadingExperience[key] / data.length / 1000;
-                let category = '';
-                if (percentile <= 0.8) {
-                    category = 'Good';
-                } else if (percentile > 0.8 && percentile <= 1.8) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
-                }
-                average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + 's',
-                    category: category,
-                    explanation:
-                        'Good (≤ 1.8 s), Needs Improvement (1.8 - 3.0 s), Poor (> 3.0 s)'
-                };
-            } else if (key === 'FIRST_INPUT_DELAY') {
-                let percentile = sum.loadingExperience[key] / data.length;
-                let category = '';
-                if (percentile <= 100) {
-                    category = 'Good';
-                } else if (percentile > 100 && percentile <= 300) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
-                }
-                average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + 'ms',
-                    category: category,
-                    explanation:
-                        'Good (≤ 100 ms), Needs Improvement (100 - 300 ms), Poor (> 300 ms)'
-                };
-            } else if (key === 'INTERACTION_TO_NEXT_PAINT') {
-                let percentile = sum.loadingExperience[key] / data.length;
-                let category = '';
-                if (percentile <= 100) {
-                    category = 'Good';
-                } else if (percentile > 100 && percentile <= 300) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
-                }
-                average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + 'ms',
-                    category: category,
-                    explanation:
-                        'Good (≤ 100 ms), Needs Improvement (100 - 300 ms), Poor (> 300 ms)'
-                };
-            } else if (key === 'LARGEST_CONTENTFUL_PAINT') {
-                let percentile = sum.loadingExperience[key] / data.length;
-                let category = '';
-                if (percentile <= 2.5) {
-                    category = 'Good';
-                } else if (percentile > 2.5 && percentile <= 4.0) {
-                    category = 'Needs Improvement';
-                } else {
-                    category = 'Poor';
-                }
-                average.loadingExperience[key] = {
-                    percentile: percentile.toFixed(2) + 's',
-                    category: category,
-                    explanation:
-                        'Good (≤ 2.5 s), Needs Improvement (2.5 - 4.0 s), Poor (> 4.0 s)'
+                    explanation: `Good (≤ ${metric.good}${metric.unit}), Needs Improvement (${metric.good}${metric.unit} - ${metric.needsImprovement}${metric.unit}), Poor (> ${metric.needsImprovement}${metric.unit})`
                 };
             }
         });
-
         return average;
     });
     res.status(200).json({
